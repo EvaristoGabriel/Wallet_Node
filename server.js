5af55c5d47ac2bd7ae48bf60fcc7e28baaf9d393
Gabriel Evaristo <gabriel.evaristo@estudante.ufjf.br>
2024-10-21 19:18:59 -0300

    fiz muita coisa

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ import fastifyCookie from 'fastify-cookie';
 import {db} from "./database/database.js"
 import bodyParser from "body-parser";
 
+
 const __dirname = path.resolve();
 const server = fastify()
+
+ // Criando a instância do RedisStore
+
 
 server.register(fastifyCookie);
 server.register(fastifySession, {
@@ -17,6 +21,7 @@ server.register(fastifySession, {
     cookie: { secure: process.env.NODE_ENV === 'production',
             httpOnly: true,
             sameSite: 'Strict',
+            maxAge: 604800000,
      }, // Altere para true se usar HTTPS
     saveUninitialized: false,
     resave: false,
@@ -111,10 +116,10 @@ server.post('/entrarC', async (request, reply) => {
 })
 
 server.get('/home', async (request, reply) =>{
-    console.log(request.session)
-    console.log('ta na home: ')
+    if (!request.session.loggedIn) {
+        return reply.redirect('/');
+    }
     const userid = request.session.userId
-    console.log(userid)
     const consultaFiis = await db.query(`
                                             SELECT COUNT(*) as soma, COALESCE(SUM(valor_compra),0.0) as vc, COALESCE(SUM(dividendos),0.0) as div 
                                             FROM fii f
@@ -146,6 +151,7 @@ server.get('/home', async (request, reply) =>{
                                             INNER JOIN usuario u on w.usercpf = u.cpf 
                                             WHERE u.id = $1;
                                         `, [userid])
+    console.log("Ta aqui")
     console.log(consultaAcoes.rows)
     const result_acoes = consultaAcoes.rows[0]
     const soma_acoes =result_acoes.soma
@@ -171,80 +177,296 @@ server.get('/home', async (request, reply) =>{
     updatedFile = updatedFile.replace("{{valor_acoes}}", valor_acoes);
     updatedFile = updatedFile.replace("{{dividendo_acoes}}", dividendos_acoes);
 
-
-
-
-
     // Envia o HTML atualizado
     return reply.type("html").send(updatedFile);
 })
 
 
-server.get('/fii', (request, reply) =>{
-    const file = readFileSync(__dirname + "/fii.html")
-    return reply.type("html").send(file)
-})
-server.post('/api/fiis', async (request, reply) => {
-    const { nome, qtdecotas, valcompra, datacompra } = JSON.parse(JSON.stringify(request.body))
-    console.log(nome+" "+qtdecotas+" "+valcompra+" "+ data);
-    try {
-        await db.query('BEGIN');
+server.get('/fii', async (request, reply) =>{
+    // if (!request.session.loggedIn) {
+    //     return reply.redirect('/');
+    // }
+    const userid = request.session.userId
+    const consultaFiis = await db.query(`
+                                            SELECT f.NOME, SUM(f.dividendos) AS DIVIDENDOS, SUM(f.valor_atual) AS VALOR_ATUAL, COUNT(f.NOME) quantidade
+                                            FROM fii f
+                                            INNER JOIN wallet w on f.walletid = w.id 
+                                            INNER JOIN usuario u on w.usercpf = u.cpf 
+                                            WHERE u.id = $1 
+                                            GROUP BY f.NOME ORDER BY f.NOME;
+                                       `, [userid]);
+                                       
+    
+    if (consultaFiis.rows.length > 0) {
+        let file = readFileSync(__dirname + "/fii.html", 'utf8'); // Certifique-se de ler como string
+      
+        // Monta o HTML com os dados dos FIIs
+        let fiisHtml = consultaFiis.rows.map(fii => `
+            <form class="form-component" action="" method="">
+                <div class="div-form-esq">
+                <input type="hidden" name="nome" value="${fii.nome}">
+                <button name="FiiDetail" class="componente" type="submit">
+                    <div class="cima">
+                    <h3 class="h3">${fii.nome}</h3>
+                    <p class="texto-fii">Valor:<br>R$${fii.valor_atual}</p>
+                    </div>
+                    <div class="baixo">
+                    <div class="baixo-esq">
+                        <p class="texto-fii">Ultimo dividendo:</p>
+                        <p class="texto-fii">R$${fii.dividendos}</p>
+                    </div>
+                    <div class="baixo-dir">
+                        <p class="texto-fii texto-fii-baixo">Quantidade de cotas: </p>
+                        <p class="texto-fii texto-cotas">${fii.quantidade}</p>
+                    </div>
+                    </div>
+                </button>
+                </div>
+                <div class="div-form-dir">
+                <ul class="ul-component">
+                    <li class="li-component"><a href="" title="Adicionar ao extrato" class="a-component"><img class="img-component" src="/public/image/addtoextract.ico">
+                    </a></li>
+                    <li class="li-component"><a href="" title="Deletar" class="a-component"><img class="img-component" src="/public/image/trash.ico">
+                    </a></li>
+                </ul>
+                </div>
+            </form>
+        `).join(''); 
 
-        for (let i = 0; i < qtdecotas; i++) {
-            await db.query(
-                'INSERT INTO fiis (nome, valor_gasto, valor_atual,data_compra) VALUES ($1, $2, $3)',
-                [nome, valcompra / qtdecotas, valcompra / qtdecotas,datacompra]
-            );
-        }
+        file = file.replace('{{fiis}}', fiisHtml);
+        return reply.type('text/html').send(file);
+      } else {
+        // Se não houver FIIs, retorna o HTML sem os boxes de FIIs
+        const file = readFileSync(__dirname + "/fii.html", 'utf8');
+        return reply.type('text/html').send(file.replace('{{fiis}}', '<p>Nenhum fundo encontrado</p>'));
+      }
+})
+// server.post('/api/fiis', async (request, reply) => {
+//     const { nome, qtdecotas, valcompra, datacompra } = JSON.parse(JSON.stringify(request.body))
+//     console.log(nome+" "+qtdecotas+" "+valcompra+" "+ data);
+//     try {
+//         await db.query('BEGIN');
 
-        // Confirmar a transação
-        await db.query('COMMIT');
-        return reply.status(201).send('Fundo Imobiliário adicionado com sucesso');
-    } catch (error) {
-        await db.query('ROLLBACK');
-        console.error('Erro ao adicionar fundo imobiliário:', error);
-        return reply.status(500).send('Erro ao adicionar fundo imobiliário');
-    }
-});
-server.get('/tesouro', (request, reply) =>{
-    const file = readFileSync(__dirname + "/tesouro.html")
-    return reply.type("html").send(file)
+//         for (let i = 0; i < qtdecotas; i++) {
+//             await db.query(
+//                 'INSERT INTO fiis (nome, valor_gasto, valor_atual,data_compra) VALUES ($1, $2, $3)',
+//                 [nome, valcompra / qtdecotas, valcompra / qtdecotas,datacompra]
+//             );
+//         }
+
+//         // Confirmar a transação
+//         await db.query('COMMIT');
+//         return reply.status(201).send('Fundo Imobiliário adicionado com sucesso');
+//     } catch (error) {
+//         await db.query('ROLLBACK');
+//         console.error('Erro ao adicionar fundo imobiliário:', error);
+//         return reply.status(500).send('Erro ao adicionar fundo imobiliário');
+//     }
+// });
+server.get('/tesouro', async(request, reply) =>{
+    // if (!request.session.loggedIn) {
+    //     return reply.redirect('/');
+    // }
+    const userid = request.session.userId
+    const consultaFiis = await db.query(`
+                                            SELECT F.*
+                                            FROM tesouro f
+                                            INNER JOIN wallet w on f.walletid = w.id 
+                                            INNER JOIN usuario u on w.usercpf = u.cpf 
+                                            WHERE u.id = $1 
+                                            ORDER BY f.NOME;
+                                       `, [userid]);
+                                       
+    
+    if (consultaFiis.rows.length > 0) {
+        let file = readFileSync(__dirname + "/tesouro.html", 'utf8'); // Certifique-se de ler como string
+      
+        // Monta o HTML com os dados dos FIIs
+        let tesouroHTML = consultaFiis.rows.map(fii => `
+            <form class="form-component" action="" method="post">
+            <div class="div-form-esq">
+                <input type="hidden" name="id" value="">
+                <button name="tesouroDetail" class="componente componente-tesouro" type="submit">
+                    <div class="cima cima-tesouro">
+                        <h3 class="h3"></h3>
+                        <p class="texto-fii">Valor Aplicado:</p>
+                    </div>
+                    <div class="baixo baixo-tesouro">
+                        <div class="baixo-esq">
+                            <p class="texto-fii">Valor Atual:</p>
+                            <p class="texto-fii">R$ </p>
+                        </div>
+                        <div class="baixo-dir baixo-dir-tesouro">
+                            <p class="texto-fii texto-fii-baixo">Valor Prometido: </p>
+                            <p class="texto-fii ">R$</p>
+                        </div>
+                    </div>
+                </button>
+            </div>
+            <div class="div-form-dir">
+                <ul class="ul-component">
+                    <li class="li-component"><a href="" title="Adicionar ao extrato" class="a-component"><img class="img-component" src="/public/image/addtoextract.ico">
+                        </a></li>
+                    <li class="li-component"><a href="" title="Deletar" class="a-component"><img class="img-component" src="/public/image/trash.ico">
+                        </a></li>
+                </ul>
+            </div>
+        </form>
+        `).join(''); 
+        
+        file = file.replace('{{tesouro}}', tesouroHTML);
+        return reply.type('text/html').send(file);
+      } else {
+        // Se não houver FIIs, retorna o HTML sem os boxes de FIIs
+        const file = readFileSync(__dirname + "/tesouro.html", 'utf8');
+        return reply.type('text/html').send(file.replace('{{tesouro}}', '<p>Nenhum tesouro encontrado</p>'));
+      }
 })
-server.get('/acoes', (request, reply) =>{
-    const file = readFileSync(__dirname + "/acoes.html")
-    return reply.type("html").send(file)
+server.get('/acoes', async(request, reply) =>{
+    // if (!request.session.loggedIn) {
+    //     return reply.redirect('/');
+    // }
+    const userid = request.session.userId
+    const consultaFiis = await db.query(`
+                                            SELECT F.*
+                                            FROM acao f
+                                            INNER JOIN wallet w on f.walletid = w.id 
+                                            INNER JOIN usuario u on w.usercpf = u.cpf 
+                                            WHERE u.id = $1 
+                                            ORDER BY f.NOME;
+                                       `, [userid]);
+                                       
+    
+    if (consultaFiis.rows.length > 0) {
+        let file = readFileSync(__dirname + "/acao.html", 'utf8'); // Certifique-se de ler como string
+      
+        // Monta o HTML com os dados dos FIIs
+        let acaoHTML = consultaFiis.rows.map(fii => `
+            <form class="form-component" action="" method="">
+                <div class="div-form-esq">
+                <input type="hidden" name="nome" value="${fii.nome}">
+                <button name="FiiDetail" class="componente" type="submit">
+                    <div class="cima">
+                    <h3 class="h3">${fii.nome}</h3>
+                    <p class="texto-fii">Valor:<br>R$${fii.valor_atual}</p>
+                    </div>
+                    <div class="baixo">
+                    <div class="baixo-esq">
+                        <p class="texto-fii">Ultimo dividendo:</p>
+                        <p class="texto-fii">R$${fii.dividendos}</p>
+                    </div>
+                    <div class="baixo-dir">
+                        <p class="texto-fii texto-fii-baixo">Quantidade de cotas: </p>
+                        <p class="texto-fii texto-cotas">${fii.quantidade}</p>
+                    </div>
+                    </div>
+                </button>
+                </div>
+                <div class="div-form-dir">
+                <ul class="ul-component">
+                    <li class="li-component"><a href="" title="Adicionar ao extrato" class="a-component"><img class="img-component" src="/public/image/addtoextract.ico">
+                    </a></li>
+                    <li class="li-component"><a href="" title="Deletar" class="a-component"><img class="img-component" src="/public/image/trash.ico">
+                    </a></li>
+                </ul>
+                </div>
+            </form>
+        `).join(''); 
+        
+        file = file.replace('{{acao}}', acaoHTML);
+        return reply.type('text/html').send(file);
+      } else {
+        // Se não houver FIIs, retorna o HTML sem os boxes de FIIs
+        const file = readFileSync(__dirname + "/acao.html", 'utf8');
+        return reply.type('text/html').send(file.replace('{{acao}}', '<p>Nenhuma ação encontrada</p>'));
+      }
 })
-server.get('/extrato', (request, reply) =>{
-    const file = readFileSync(__dirname + "/extrato.html")
-    return reply.type("html").send(file)
+server.get('/extrato', async(request, reply) =>{
+    // if (!request.session.loggedIn) {
+    //     return reply.redirect('/');
+    // }
+    const userid = request.session.userId
+    const consultaFiis = await db.query(`
+                                            SELECT f.NOME, SUM(f.dividendos) AS DIVIDENDOS, SUM(f.valor_atual) AS VALOR_ATUAL, COUNT(f.NOME) quantidade
+                                            FROM fii f
+                                            INNER JOIN wallet w on f.walletid = w.id 
+                                            INNER JOIN usuario u on w.usercpf = u.cpf 
+                                            WHERE u.id = $1 
+                                            GROUP BY f.NOME ORDER BY f.NOME;
+                                       `, [userid]);
+                                       
+    
+    if (consultaFiis.rows.length > 0) {
+        let file = readFileSync(__dirname + "/fii.html", 'utf8'); // Certifique-se de ler como string
+      
+        // Monta o HTML com os dados dos FIIs
+        let fiisHtml = consultaFiis.rows.map(fii => `
+            <form class="form-component" action="" method="">
+                <div class="div-form-esq">
+                <input type="hidden" name="nome" value="${fii.nome}">
+                <button name="FiiDetail" class="componente" type="submit">
+                    <div class="cima">
+                    <h3 class="h3">${fii.nome}</h3>
+                    <p class="texto-fii">Valor:<br>R$${fii.valor_atual}</p>
+                    </div>
+                    <div class="baixo">
+                    <div class="baixo-esq">
+                        <p class="texto-fii">Ultimo dividendo:</p>
+                        <p class="texto-fii">R$${fii.dividendos}</p>
+                    </div>
+                    <div class="baixo-dir">
+                        <p class="texto-fii texto-fii-baixo">Quantidade de cotas: </p>
+                        <p class="texto-fii texto-cotas">${fii.quantidade}</p>
+                    </div>
+                    </div>
+                </button>
+                </div>
+                <div class="div-form-dir">
+                <ul class="ul-component">
+                    <li class="li-component"><a href="" title="Adicionar ao extrato" class="a-component"><img class="img-component" src="/public/image/addtoextract.ico">
+                    </a></li>
+                    <li class="li-component"><a href="" title="Deletar" class="a-component"><img class="img-component" src="/public/image/trash.ico">
+                    </a></li>
+                </ul>
+                </div>
+            </form>
+        `).join(''); 
+        
+        file = file.replace('{{fiis}}', fiisHtml);
+        return reply.type('text/html').send(file);
+      } else {
+        // Se não houver FIIs, retorna o HTML sem os boxes de FIIs
+        const file = readFileSync(__dirname + "/fii.html", 'utf8');
+        return reply.type('text/html').send(file.replace('{{fiis}}', '<p>Nenhum fundo encontrado</p>'));
+      }
 })
 
-server.post('/fii', (request, reply) =>{
-    const {nome} = JSON.parse(JSON.stringify(request.body))
-    // nome.create({
-    //     nome,
-    // })
-    const file = readFileSync(__dirname + "/fii.html")
-    return reply.type("html").send(file)
-})
+// server.post('/fii', (request, reply) =>{
+//     const {nome} = JSON.parse(JSON.stringify(request.body))
+//     // nome.create({
+//     //     nome,
+//     // })
+//     const file = readFileSync(__dirname + "/fii.html")
+//     return reply.type("html").send(file)
+// })
 
-server.post('/tesouro', (request, reply) =>{
-    const {nome} = JSON.parse(JSON.stringify(request.body))
-    // nome.create({
-    //     nome,
-    // })
-    const file = readFileSync(__dirname + "/tesouro.html")
-    return reply.type("html").send(file)
-})
+// server.post('/tesouro', (request, reply) =>{
+//     const {nome} = JSON.parse(JSON.stringify(request.body))
+//     // nome.create({
+//     //     nome,
+//     // })
+//     const file = readFileSync(__dirname + "/tesouro.html")
+//     return reply.type("html").send(file)
+// })
 
-server.post('/acoes', (request, reply) =>{
-    const {nome} = JSON.parse(JSON.stringify(request.body))
-    // nome.create({
-    //     nome,
-    // })
-    const file = readFileSync(__dirname + "/acoes.html")
-    return reply.type("html").send(file)
-})
+// server.post('/acoes', (request, reply) =>{
+//     const {nome} = JSON.parse(JSON.stringify(request.body))
+//     // nome.create({
+//     //     nome,
+//     // })
+//     const file = readFileSync(__dirname + "/acoes.html")
+//     return reply.type("html").send(file)
+// })
 
 server.listen({port: 3333,
 })
